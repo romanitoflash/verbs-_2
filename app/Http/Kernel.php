a9f410899f1458c2c49932af082f2808fab17721
romanitoflash <romanito0809@hotmail.com>
2024-08-15 12:07:25 -0600

    version admini usuarios
    versión con roles y usuarios

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,5 +69,5 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,  
-    ];
+    ]; 
 }
